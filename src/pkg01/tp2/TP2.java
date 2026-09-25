944a3b6c6690721780ba86c237d969d3cb8b345c
obesnier <obesnier@350028R-F115X13.lise.kreizenn.bzh>
2026-09-25 11:36:44 +0200

    premier test

---
+++ src/pkg01/tp2/TP2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg01.tp2;
+
+/**
+ *
+ * @author obesnier
+ */
+public class TP2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // test github
+    }
+    
+}
